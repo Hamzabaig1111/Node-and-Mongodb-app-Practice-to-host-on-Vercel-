c772d17973d6b30391dd7107a6a851b06e25b801
Muhammad Hamza Baig <41506373+Hamzabaig1111@users.noreply.github.com>
2024-03-29 11:51:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const PORT = process.env.PORT || 3000;
 // Connect to MongoDB to connect with live Mongodb
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000
+  useUnifiedTopology: true
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
